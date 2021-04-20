a686ced1ff1cefab79e67f37e6597ff74eb37286
finley996 <fang.ly@qq.com>
2021-04-20 17:36:25 +0800

    add mouse control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,35 +20,57 @@ public class Player : MonoBehaviour
 
     public Transform m_explosionFX;
 
+    protected Vector3 m_targetPos;
+    
+    public LayerMask m_inputMask;
+
 
     private void Start()
     {
         m_transform = this.transform;
         m_audio = this.GetComponent<AudioSource>();
+        m_targetPos = this.m_transform.position;
+    }
+
+    void MoveTo()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 ms = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(ms);
+            RaycastHit hitinfo;
+            bool iscast = Physics.Raycast(ray, out hitinfo, 1000, m_inputMask);
+            if (iscast)
+            {
+                m_targetPos = hitinfo.point;
+            }
+        }
+        Vector3 pos = Vector3.MoveTowards(m_transform.position, m_targetPos, m_speed * Time.deltaTime);
+        m_transform.position = pos;
     }
     void Update()
     {
-        float move_vertical = 0;
-        float move_horizon = 0;
-        float move = m_speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            move_vertical += move;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            move_vertical -= move;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            move_horizon -= move;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            move_horizon += move;
-        }
-        m_transform.Translate(new Vector3(move_horizon, 0, move_vertical));
-
+        //float move_vertical = 0;
+        //float move_horizon = 0;
+        //float move = m_speed * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    move_vertical += move;
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    move_vertical -= move;
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    move_horizon -= move;
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    move_horizon += move;
+        //}
+        //m_transform.Translate(new Vector3(move_horizon, 0, move_vertical));
+        MoveTo();
         m_rocketTimer -= Time.deltaTime;
         if (m_rocketTimer <= 0)
         {
